9c157aadb6a75bf04f7a9c415a4d5a25e3924857
Hricha Jaiswal <99092506+Hricha-1511@users.noreply.github.com>
2026-03-05 19:59:13 +0530

    Create Check_Even_no.py

---
+++ Check_Even_no.py
@@ -0,0 +1,9 @@
+# Check if the given number is even
+
+def check_even(n):
+    if n % 2 == 0:
+        print(f"{n} is an Even number")
+    else:
+        print(f"{n} is not an Even number")
+
+check_even(4)
