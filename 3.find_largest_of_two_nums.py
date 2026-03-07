04b2260df58b21e4026a50ab11ea9b3e0b0587ce
Hricha Jaiswal <99092506+Hricha-1511@users.noreply.github.com>
2026-03-07 12:30:55 +0530

    Create 3.find_largest_of_two_nums.py

---
+++ 3.find_largest_of_two_nums.py
@@ -0,0 +1,11 @@
+# Python code to print the largest of the two numbers, takes user input and output which one os the largest.
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print("Largest number is:", num1)
+else:
+    print("Largest number is:", num2)
+
